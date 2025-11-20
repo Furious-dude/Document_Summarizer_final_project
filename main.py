12b5e4b012c0afebe2fe9d9d4f837331ea0fb3a0
ML Hiep <100101scucide@gmail.com>
2025-11-21 00:07:44 +0700

    minor changes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 from text_chunker import split_into_paragraphs
 from PIL import ImageTk
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
 # global summary_cache
@@ -17,23 +17,22 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        # init state values
+    # init state values
         self.is_loading_pdf = False
         self.is_loading_model = False
         self.is_summarizing = False
 
-        # configure window
+    # configure window
         self.title("Document summerizer phase 2")
         self.geometry(f"{1100}x{580}")
 
-        # configure grid layout (4x4)
+    # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # main se gom 3 phan : 3 cot,2 hang. va co 2 che do(tam thoi)
 
-        # create sidebar frame with widgets
+    # create sidebar frame with widgets
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
@@ -45,13 +44,14 @@ class App(customtkinter.CTk):
         self.pdf_file_button.grid(row=2, column=0, padx=20, pady=10)
         self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+        
+    # customization menus
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
         self.language_mode_option_menu = customtkinter.CTkOptionMenu(self.sidebar_frame,values=["English","Tiếng Việt"])
         self.language_mode_option_menu.grid(row=4,column=0,padx=20,pady=(10,10))
-        
-        # set  appearance menu
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+                
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark" ,"System"],
                                                                        command=self.change_appearance_mode_event)        
         self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
@@ -60,42 +60,53 @@ class App(customtkinter.CTk):
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
-        # create main entry and button
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry") #keep
-        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+        
 
-        self.summarize_button = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
-        self.summarize_button.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew") 
+        
+    # create checkboxes
+        self.checkbox_slider_frame = customtkinter.CTkFrame(self)
+        self.checkbox_slider_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.summarize_button = customtkinter.CTkButton(master=self.checkbox_slider_frame, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
+        self.summarize_button.grid(row=0, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew") 
+        self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
         
         #this is the button next to the main entry box
         
 
-        # textbox main section
+    # textbox main section # indent de tao chapter, rut gon code
+        self.textbox_raw_bounding_box = customtkinter.CTkFrame(self,width=250)
+        self.textbox_raw_bounding_box.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
         self.textbox_raw = customtkinter.CTkTextbox(self, width=250)
         self.textbox_raw.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.textbox_raw.configure(border_width=2,corner_radius=8,fg_color='transparent',border_color="grey")
+        self.textbox_raw.configure(border_width=2,corner_radius=8,fg_color='transparent',border_color="grey",scrollbar_button_hover_color="green")
+        self.entry_label = customtkinter.CTkLabel(self,text="Raw text")
+        self.entry_label.grid(row=2, column=1, padx=20, pady=(20, 10))
 
-
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
         # tabview related
         # self.tabview = customtkinter.CTkTabview(self, width=250)
         # self.tabview.grid(row=0, column=2, columnspan=2,padx=(20, 0), pady=(20, 0), sticky="nsew")
         
         self.textbox_summarized = customtkinter.CTkTextbox(self,width=250)
         self.textbox_summarized.grid(row=0,column=2, columnspan=2,padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.textbox_summarized.configure(border_width=2,corner_radius=8,fg_color='transparent',border_color="grey")
+        self.textbox_summarized.configure(border_width=2,corner_radius=8,fg_color='transparent',border_color="grey",scrollbar_button_hover_color="green")
         
 
-        # create scrollable frame
+    # create scrollable frame
         self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="CTkScrollableFrame")
         self.scrollable_frame.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
-        self.scrollable_frame_switches = []
-        for i in range(100):
-            switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i}")
-            switch.grid(row=i, column=0, padx=10, pady=(0, 20))
-            self.scrollable_frame_switches.append(switch)
+        self.scrollable_frame_switch_shorten = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"Shorten mode")
+        self.scrollable_frame_switch_shorten.grid(row=2, column=0, padx=10, pady=(0, 20))
+        self.scrollable_frame_switch_summerizer = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"Summerize")
+        self.scrollable_frame_switch_summerizer.grid(row=3, column=0, padx=10, pady=(0, 20))
+        
+        
 
-        # create slider and progressbar frame for decor
+    # create slider and progressbar frame for decor
         self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
         self.slider_progressbar_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
         self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
@@ -116,20 +127,10 @@ class App(customtkinter.CTk):
         self.progressbar_1.configure(mode="indeterminnate")
         self.progressbar_1.start()
 
-        # create checkboxes
-        self.checkbox_slider_frame = customtkinter.CTkFrame(self)
-        self.checkbox_slider_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
-        self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
-        self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
+        
 
-        # set default values        
-        self.checkbox_1.select()
-        self.scrollable_frame_switches[0].select()
-        self.scrollable_frame_switches[4].select()
+    # set default values        
+        
         self.appearance_mode_optionemenu.set("Dark")
         self.scaling_optionemenu.set("100%")
 
@@ -138,10 +139,9 @@ class App(customtkinter.CTk):
         # sap xep lai vi tri initiate cac cot, va cac thanh phan lien quan den cot do
 
 
-        # set actual default values and function calls by me
+    # set actual default values and function calls by me
         self.sidebar_button_3.configure(text="Chunk text into paragraph",command=self.get_paragraph)
         self.txt_file_button.configure(text="Choose text file")
-        self.checkbox_3.configure(state="disabled")
         self.summarize_button.configure(command=self.text_summerize, text="Summarize")
         self.pdf_file_button.configure(command=self.open_pdf,text="Open pdf file")
 
@@ -292,3 +292,17 @@ if __name__ == "__main__":
         # self.radio_button_3.configure(state="disabled")
         # self.optionmenu_1.set("CTkOptionmenu")
         # self.combobox_1.set("CTkComboBox")
+        # self.checkbox_1.select()
+        # self.scrollable_frame_switches[0].select()
+        # self.scrollable_frame_switches[4].select()
+        # create main entry and button
+        # self.entry_tab = customtkinter.CTkFrame(self,corner_radius=4,width=140)
+        # self.entry_tab.grid(row=0,column=1,rowspan=2,sticky="nsew")
+        # self.entry_tab.grid_rowconfigure(3)
+        # self.entry = customtkinter.CTkEntry(self.entry_tab, placeholder_text="enter custom command") #keep maybe
+        # self.entry.grid(row=1, column=0, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+        # self.scrollable_frame_switches = []
+        # for i in range(100):
+        #     switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i}")
+        #     switch.grid(row=i, column=0, padx=10, pady=(0, 20))
+        #     self.scrollable_frame_switches.append(switch)
